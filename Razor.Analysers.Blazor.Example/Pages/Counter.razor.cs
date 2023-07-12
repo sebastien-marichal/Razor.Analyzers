7adbb1ae713a8d3edde9601a6ff0b9322fd7532d
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2023-07-12 11:30:00 +0200

    Rule with RegisterSyntaxNodeAction only

--- Razor.Analysers.Blazor.Example/Pages/Counter.razor.cs
+++ Razor.Analysers.Blazor.Example/Pages/Counter.razor.cs
@@ -1,19 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
-using System.Net.Http;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.Components.Routing;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.Web.Virtualization;
-using Microsoft.JSInterop;
-using Razor.Analysers.Blazor.Example;
-using Razor.Analysers.Blazor.Example.Shared;
-
 namespace Razor.Analysers.Blazor.Example.Pages
 {
     public partial class Counter
