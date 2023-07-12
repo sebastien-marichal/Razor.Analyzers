57a40211f7a259d7c4658ea8bf56e9e94f44745a
Costin Zaharia <costin.zaharia@sonarsource.com>
2023-07-12 16:14:35 +0200

    Add Razor net5 sample

---
+++ Razor.Analyzers.Razor.Net5.Example/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Razor.Analyzers.Razor.Net5.Example
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
